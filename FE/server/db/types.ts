87c361f69e5f60ced9fdd66211cfa9bf0dd3a1f5
NotMyTata <tata.wiropati@gmail.com>
2026-06-05 10:26:39 +0700

    fix: UserWithRole

--- FE/server/db/types.ts
+++ FE/server/db/types.ts
@@ -7,10 +7,10 @@ export interface UserTable {
   role_id: number
   name: string
   email: string
-  password: string
+  password_hash: string
+  last_login_at: Date | null
   readonly created_at: Generated<Date>
   readonly updated_at: Generated<Date>
-  last_login_at?: Date | null
 }
 export type UserSelect = Selectable<UserTable>
 export type UserInsert = Insertable<UserTable>
@@ -78,6 +78,19 @@ export interface Database {
   identifications: IdentificationTable
   images: ImageTable
 }
+
+export interface UserWithRole {
+  id: Generated<number>;
+  role_id: number;
+  role_name: string;
+  name: string;
+  email: string;
+  password_hash: string;
+  last_login_at: Date | null;
+  created_at: Generated<Date>;
+  updated_at: Generated<Date>;
+}
+export type UserWithRoleSelect = Selectable<UserWithRole>;
 
 export interface EnrichedIdentification {
   readonly id: number
